b5f9351815e3614a6410a3e74bab01fb1db3836b
oott123 <git@public.oott123.com>
2017-07-13 15:56:07 +0800

    pass eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-var webpack = require('webpack');
-var isDev = process.env.NODE_ENV !== 'production';
+var webpack = require('webpack')
+var isDev = process.env.NODE_ENV !== 'production'
 
 var config = {
-    devtool: isDev && '#source-map',
-    entry: {
-        index: './src/proxy/assets/index.js'
-    },
-    output: {
-        path: './src/proxy/assets',
-        filename: '[name].bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: "babel-loader?presets[]=es2015"
-        }]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            ENV: JSON.stringify(process.env.NODE_ENV)
-        })
-    ]
-};
-
-if (!isDev) {
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            sourceMap: false
-        })
-    );
+  devtool: isDev && '#source-map',
+  entry: {
+    index: './src/proxy/assets/index.js'
+  },
+  output: {
+    path: './src/proxy/assets',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader?presets[]=es2015'
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 }
 
-module.exports = config;
+if (!isDev) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      sourceMap: false
+    })
+  )
+}
+
+module.exports = config
