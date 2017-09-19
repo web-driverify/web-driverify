b0ea4f0bf6e1b01ffc8532ed1cb4f493443537ee
oott123 <git@public.oott123.com>
2017-09-19 18:46:36 +0800

    plugins

--- src/endpoints/export.js
+++ src/endpoints/export.js
@@ -11,16 +11,16 @@ glob.sync(path.join(__dirname, '*/*.js'))
     Endpoint.register(require(endpoint).default)
   })
 // require plugins
-glob.sync(path.join(__dirname, '../../node_modules/web-driverify-endpoint-*'))
-  .forEach((pluginPath) => {
-    let pluginName = path.basename(pluginPath)
-    let conf = config.plugin[pluginName]
-    if (conf.disable) {
-      return
-    }
-    let plugin = require(pluginPath)
+config.plugins
+.filter((plugin) => plugin.enable)
+.forEach((plugin) => {
+  try {
+    let plugin = require(plugin.id)
     if (plugin.default) {
       plugin = plugin.default
     }
     plugin({Endpoint, env, config: conf})
-  })
+  } catch (err) {
+    console.error(`Failed to load plugin: ${plugin.id}`, err)
+  }
+})
