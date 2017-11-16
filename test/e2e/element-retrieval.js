9eae50026feb4facf41bbb3291acc5962ebec3b3
harttle <harttle@harttle.com>
2017-11-16 16:26:52 +0800

    test cases for wdio $, $$, close #5 spec: http://webdriver.io/api/utility/$$.html

--- test/e2e/element-retrieval.js
+++ test/e2e/element-retrieval.js
@@ -6,27 +6,34 @@ import chai from 'chai'
 let expect = chai.expect
 
 describe('element retrieval', function () {
-  var plain = `${config.stub.url}/plain.html`
-
   before(function () {
-    browser.url(plain)
+    browser.url(`${config.stub.url}/element-retrieval.html`)
   })
 
   it('POST /session/:sessionId/element', function () {
-    var val = browser.element('.control-label')
+    var val = browser.element('p.lead')
     expect(val.value).to.have.property('ELEMENT')
   })
 
   it('POST /session/:sessionId/element NoSuchElement', function () {
-    var val = browser.element('.foo')
+    var val = browser.element('.not-exist')
     expect(val).to.have.property('type', 'NoSuchElement')
     expect(val).to.have.property('sessionId')
     expect(val).to.have.property('message')
   })
 
-  it('POST /session/:sessionId/elements', function () {
-    var val = browser.elements('.control-label')
-    expect(val.value).to.have.lengthOf(1)
-    expect(val.value[0]).to.have.property('ELEMENT')
+  describe('POST /session/:sessionId/elements', function () {
+    it('should get elements', function () {
+      var val = browser.elements('.lead')
+      expect(val.value).to.have.lengthOf(2)
+      expect(val.value[0]).to.have.property('ELEMENT')
+    })
+    it('should support $ and $$', function () {
+      var menu = $('#menu')
+      let text = menu.$$('li')[2].getText('a')
+      expect(text).to.equal('API')
+      menu.$$('li')[2].$('a').getText()
+      expect(text).to.equal('API')
+    })
   })
 })
